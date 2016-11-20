8e42ca6d018c596c42c1c82adbd913b0bae9799f
sbociot <stevebocicot@icloud.com>
2016-11-20 08:57:09 -0500

    "script tabs edit"

--- script/myscript.js
+++ script/myscript.js
@@ -1,22 +1,18 @@
 "use strict";
 /* tabContent part */
-var promo1 = P86_TP_forfaits[1]
-function openForfait(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+$(document).ready(function() {
+    $(".tabs-menu a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("href");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
+});
 //ajout du nom des forfaits
 $("#croissiere h3").html(P86_TP_forfaits[1].nom);
 $("#hiver h3").html(P86_TP_forfaits[3].nom);
 $("#nature h3").html("");
-$("#forfait_promo h3").html(P86_TP_forfaits[1].nom);
+
 $("#croissiere h3")
