8a69a505f3ed2ba2657b0edafea82e1a09966369
sbociot <stevebocicot@icloud.com>
2016-11-20 17:46:21 -0500

    "forfait dans tab-3"

--- script/myscript.js
+++ script/myscript.js
@@ -15,12 +15,14 @@ $('#tab-1 p').text(P86_TP_forfaits[0].description);
 $('#tab-1 h3').text(P86_TP_forfaits[0].nom);
 $('#tab-2 p').text(P86_TP_forfaits[8].description);
 $('#tab-2 h3').text(P86_TP_forfaits[8].nom);
+$('#tab-3 p').text(P86_TP_forfaits[3].description);
+$('#tab-3 h3').text(P86_TP_forfaits[3].nom);
 $('.descriptionForfait li').first().text(P86_TP_forfaits[0].debut_saison);
 $('.descriptionForfait li:nth-child(2)').text(P86_TP_forfaits[0].mois_basse_saison);
-$('.descriptionForfait li:nth-child(3)').text('Prix du forfaits :' + ' ' + P86_TP_forfaits[0].prix_basse_saison);
+$('.descriptionForfait li:nth-child(3)').text('Prix du forfaits :' + ' '+ '$' +' '+ P86_TP_forfaits[0].prix_basse_saison);
 $('#tab-2 .descriptionForfait li').first().text(P86_TP_forfaits[8].debut_saison);
-$('.descriptionForfait li:nth-child(2)').text(P86_TP_forfaits[8].mois_basse_saison);
+$('#tab-2 .descriptionForfait li:nth-child(2)').text(P86_TP_forfaits[8].mois_basse_saison);
+$('#tab-2 .descriptionForfait li:nth-child(3)').text('Prix du forfaits :' + ' ' + '$'+' ' + P86_TP_forfaits[8].prix_basse_saison);
 //fin de l'ajout des forfait dans tab_content
-
 
 
