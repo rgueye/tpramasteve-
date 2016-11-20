9fdc78db07292d2b968ab3ba41ab723bbd124847
sbociot <stevebocicot@icloud.com>
2016-11-20 13:02:59 -0500

    "ajout du prix"

--- script/myscript.js
+++ script/myscript.js
@@ -17,6 +17,9 @@ $('#tab-2 p').text(P86_TP_forfaits[8].description);
 $('#tab-2 h3').text(P86_TP_forfaits[8].nom);
 $('.descriptionForfait li').first().text(P86_TP_forfaits[0].debut_saison);
 $('.descriptionForfait li:nth-child(2)').text(P86_TP_forfaits[0].mois_basse_saison);
+$('.descriptionForfait li:nth-child(3)').text('Prix du forfaits :' + ' ' + P86_TP_forfaits[0].prix_basse_saison);
+$('#tab-2 .descriptionForfait li').first().text(P86_TP_forfaits[8].debut_saison);
+$('.descriptionForfait li:nth-child(2)').text(P86_TP_forfaits[8].mois_basse_saison);
 //fin de l'ajout des forfait dans tab_content
 
 
